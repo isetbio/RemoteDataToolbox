fac69901b4b713b1993394166d35970f05643ffc
Brian Wandell <wandell@stanford.edu>
2015-08-12 14:05:09 -0700

    Adjusted listing of the files to return a string if needed.

--- @rdata/listFiles.m
+++ @rdata/listFiles.m
@@ -1,4 +1,4 @@
-function listFiles(rd,dirName)
+function val = listFiles(rd,dirName)
 % List all the directories at the remote data site
 %
 %   rd.listFiles(dirName)
@@ -21,11 +21,23 @@ end
 
 % Find the first match and print it and return.
 % Maybe we should check for more matches ...?
-for ii=1:numel(lst)
-    if ~isempty(lst{ii})
-        fprintf('\nFiles in directory %s\n',dNames{ii});
-        disp(rd.files{ii})
-        return;
+if nargout == 0
+    for ii=1:numel(lst)
+        if ~isempty(lst{ii})
+            fprintf('\nDirectory: %s\n',rd.directories{ii});
+            for jj=1:numel(rd.files{ii})
+                fprintf('\t%s\n',char(rd.files{ii}(jj)));
+            end
+            return;
+        end
+    end
+    fprintf('\n');
+else
+    for ii=1:numel(lst)
+        val = sprintf('Files in directory %s\n',dNames{ii});
+        for jj=1:numel(rd.files{ii})
+            val = addText(val,sprintf('\t %s\n',char(rd.files{ii}(jj))));
+        end
     end
 end
 
