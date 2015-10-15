dada10820221c8edfc1768e6fc166ba0a3db1aa0
Ben Heasly <benjamin.heasly@gmail.com>
2015-10-14 22:08:56 -0400

    revise tutorial script

--- examples/project/rdtExampleProjectPublishData.m
+++ examples/project/rdtExampleProjectPublishData.m
@@ -1,21 +1,22 @@
 %%% RemoteDataToolbox Copyright (c) 2015 The RemoteDataToolbox Team.
 %
-% This is a step-by step example for working with the Remote Data Toolbox.
+% This is a tutorial for working with the Remote Data Toolbox.
 %
 % This script shows how you might generate some data as part of a project
-% and publish that data to a Maven repository as an artifact.
+% and publish that data as an artifact to the project's Maven repository.
 %
 % This script uses a JSON file to configure the Remote Data Toolbox with
-% things like the Url of your server.  This simplifies all the calls to the
-% Remote Data Toolbox functions.
+% things like the Url of the project's Maven repository.  This simplifies
+% various calls to the Remote Data Toolbox functions.
 %
-% You probably don't want to store your repository account credentials in a
-% JSON file, because others would be able to read it.  So before publishing
-% data, this script will prompt you to enter a password into a dialog box.
-% For the purposes of this demo, you should enter the password "pa55w0rd".
+% You probably don't want to store your project's repository credentials in
+% a JSON file, because others would be able to read it.  So before
+% publishing data, this script will prompt you to enter a password into a
+% dialog window.  For this demo, use the user name "demo" and password
+% "pa55w0rd".
 %
-% See the rdtExampleProjectReadData to see how to read data that's already
-% been published.
+% See the rdtExampleProjectReadData.m to see how to read data that's
+% already been published.
 %
 % @ingroup examples
 
@@ -25,9 +26,12 @@ clc;
 %% Generate an image.
 
 % generate some random data and save it as an image.
-imageData = uint8(100 * eye(500) + randi(100, [500 500]));
+row = 1 + floor((0:499) / 50);
+indexes = repmat(row, 500, 1);
+colors = jet(10);
+colors = colors(randperm(10), :);
 imageFile = fullfile(tempdir(), 'my-image.png');
-imwrite(imageData, imageFile, 'png');
+imwrite(indexes, colors, imageFile, 'png');
 
 % view the image
 imshow(imageFile)
@@ -38,7 +42,7 @@ imshow(imageFile)
 thisFolder = fileparts(which('rdtExampleProjectPublishData'));
 cd(thisFolder);
 
-% prompt for credentials to complete the configuration
+% enter credentials (demo:pa55w0rd) to complete the configuration
 configuration = rdtCredentialsDialog();
 
 %% Publish the image as an artifact.
@@ -52,15 +56,17 @@ artifactId = 'demo-image';
 % each artifact must have a version, the default is version '1'
 version = '42';
 
-% supply the configuration, which contains publishing credentials
+% supply the configuration, which now contains publishing credentials
 artifact = rdtPublishArtifact(imageFile, ...
     groupId, ...
     artifactId, ...
     version, ...
     configuration);
 
-% see metadata about the new artifact!
+%% See metadata about the new artifact!
 disp(artifact)
 
-% see the artifact in your browser!
+%% Visit the new artifact on the web!
+
+% you may need to enter your credentials again (demo:pa55w0rd)
 web(artifact.url, '-browser')
